af7e7b42cb5654a3a0a4daf6143b09ff21d76f10
Ghost <75214924+GhostService@users.noreply.github.com>
2026-06-01 12:20:07 -0500

    Actually make it work good.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const { App } = require("@slack/bolt");
+const axios = require("axios");
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -13,6 +14,28 @@ app.command("/gst-ping", async ({ command, ack, respond }) => {
   await ack();
   const latency = Date.now() - start;
   await respond({ text: `Ping successful!\nLatency: ${latency}ms` });
+});
+
+app.command("/gst-help", async ({ ack, respond }) => {
+  await ack();
+  await respond({
+    text:
+`Available Commands:
+/gst-help - This command of course
+/gst-ping - Checks bot status and latency
+/gst-fact - Useless unfunny facts`
+  });
+});
+
+app.command("/gst-fact", async ({ ack, respond }) => {
+  await ack();
+
+  try {
+    const response = await axios.get("https://uselessfacts.jsph.pl/api/v2/facts/random");
+    await respond({ text: `Since you asked...\n${response.data.text}\nThank ${response.data.source} for this unfunny fact.` });
+  } catch (err) {
+    await respond({ text: "Larp failed. Try again later." });
+  }
 });
 
 (async () => {
